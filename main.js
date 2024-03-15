6a4b03c65f0b7621036ee015754fe3cef092f9fb
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-15 18:14:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//author: [ Usama Muzammil];
+//date: [25 feb 2024];
+var favouritenumber = 50;
+console.log("This is my favourite number is ".concat(favouritenumber));
---
+++ main.ts
@@ -0,0 +1,5 @@
+//author: [ Usama Muzammil];
+//date: [25 feb 2024];
+
+let favouritenumber: number = 50;
+console.log(`This is my favourite number is ${favouritenumber}`);
